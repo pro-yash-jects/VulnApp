625a247a122b2309257799ba71b54668b25960c4
DM-DevSecOps <dm-devsecops@Mac-mini.local>
2026-09-01 11:23:36 +0530

    fix: revert internal container port to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8002;
+const PORT = process.env.PORT || 3000;
 
 // 🚨 VULNERABILITY 1: Hardcoded Secrets (Will be caught by Gitleaks)
 
